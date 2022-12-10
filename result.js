a3500a21dd01a7ba478c1930a6fc58cf869984e3
darrenjon <darrenlin23@gmail.com>
2022-12-10 23:42:50 +0800

    feat: store scores in localStorage

--- result.js
+++ result.js
@@ -2,6 +2,10 @@ const username = document.getElementById('username')
 const saveScoreBtn = document.getElementById('saveScoreBtn')
 const finalScore = document.getElementById('finalScore')
 const recentScore = localStorage.getItem('recentScore')
+// only store 5 scores
+const MAX_HIGH_SCORES = 5
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 finalScore.innerText = recentScore
 
@@ -11,4 +15,18 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
   e.preventDefault();
+
+  const score = {
+    score: recentScore,
+    name: username.value,
+  }
+  // arrange score ranking and store only 5
+  highScores.push(score)
+  highScores.sort((a, b) => b.score - a.score)
+  highScores.splice(5)
+  console.log(highScores)
+
+  // store in localStorage
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/')
 }
